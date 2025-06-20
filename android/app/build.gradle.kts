42560d900ecdcd243bbc42fad2d36d2734f7db3c
Tridip Bhowmik <tridipbhowmik898@gmail.com>
2025-06-20 11:26:39 +0600

    fix: set NDK version to 27.0.12077973

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.brainstation.assessment.brain_station_assessment_task"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
